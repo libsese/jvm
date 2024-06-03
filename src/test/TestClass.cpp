74d6b3437c1cf63a026eba5bd92dc0607fa7d89c
Kaoru <shiinasama2001@gmail.com>
2024-06-03 18:39:01 +0800

    单元测试路径自动化

--- src/test/TestClass.cpp
+++ src/test/TestClass.cpp
@@ -3,18 +3,16 @@
 #include <sese/Log.h>
 #include <sese/util/Exception.h>
 
-#define CLASS_FILE "D:/workspaces/jvm/src/test/resource/Hello.class"
-
 TEST(TestClass, Parse) {
     try {
-        auto cl = jvm::ClassLoader::loadFromFile(CLASS_FILE);
+        auto cl = jvm::ClassLoader::loadFromFile(PATH_TO_HELLO_CLASS);
     } catch (sese::Exception &exception) {
         exception.printStacktrace();
     }
 }
 
 TEST(TestClass, Getter) {
-    auto cl = jvm::ClassLoader::loadFromFile(CLASS_FILE);
+    auto cl = jvm::ClassLoader::loadFromFile(PATH_TO_HELLO_CLASS);
     // SESE_DEBUG("%s %s", cl->getSuperName().c_str(), cl->getThisName().c_str());
     EXPECT_EQ(cl->getSuperName(), "java/lang/Object");
     EXPECT_EQ(cl->getThisName(), "Hello");
